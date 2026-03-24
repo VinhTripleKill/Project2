0c9e777f301a52ed09d3eeed151e53b3e11185e4
Dr.Bright <elysiazuize@gmail.com>
2026-03-24 10:52:03 +0700

    Update ChartData.cs

--- Assets/Scripts/ChartData.cs
+++ Assets/Scripts/ChartData.cs
@@ -12,7 +12,7 @@ public class NoteData
 {
     public int lane;
     public float hitTime;
-    public float endTime; // HOLDgcvyhgsvgvchg
+    public float endTime; // HOLD
     
 
     public SlideNode[] slideNodes; // slide
